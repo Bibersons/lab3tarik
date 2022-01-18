62611dc4dda3657af379afe186a10266a547c26c
tarok <tarok@DESKTOP-VKGIL36>
2022-01-18 20:39:52 +0300

    Proje dosyası ekle.

---
+++ yikes/yikes.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	int x, n;
+	double sum = 1;
+	double add = 0;
+
+	cout << "Input n: ";
+	cin >> n;
+	cout << "Input x: ";
+	cin >> x;
+
+	if (x <= 0)
+	{
+		for (int i = 0; i <= n - 1; i++)
+		{
+			sum = sum * (i * i * i) + i;
+			cout << sum << endl;
+		}
+	}
+	else
+	{
+		for (int i = 0; i <= n - 1; i++)
+		{
+			for (int j = 0; j <= n - 1; j++)
+			{
+				add = add + (x / i + j);
+			}
+		}
+		cout << add;
+	}
+	return 0;
+}
